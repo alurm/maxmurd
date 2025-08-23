7582cc0192366b91f2e16768a5873431d6b6eb7d
ProggerX <x@proggers.ru>
2025-08-23 23:11:43 +0300

    Make haskell code alive

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
+import qualified Data.ByteString as BS
+import Data.FileEmbed (embedFileRelative)
+import Data.Text.Encoding
+import Data.Text.IO as TIO
+
+dFile :: BS.ByteString
+dFile = $(embedFileRelative "README.md")
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = TIO.putStrLn $ decodeUtf8 dFile
